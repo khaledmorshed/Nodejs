164d9936e65b8b0484fccb4d07f373879cbaf3b5
khaledmorshed <s.m.morshed10@gmail.com>
2025-05-03 01:48:46 +0600

    add user

--- chat-application/router/usersRouter.js
+++ chat-application/router/usersRouter.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const { getUsers } = require('../controller/usersController');
+const { getUsers, addUser, removeUser } = require('../controller/usersController');
 const decorateHtmlResponse  = require('../midlewares/common/decorateHtmlResponse');
+const avatarUpload = require('../midlewares/users/avaterUpload');
+const { check } = require('express-validator');
+const { addUserValidators, addUserValidationHandler } = require('../midlewares/users/usersValidator');
 const router = express.Router();
 
 
 
 // get users
 router.get('/', decorateHtmlResponse("Users"), getUsers);
+
+// add users
+router.post('/', avatarUpload, addUserValidators, addUserValidationHandler, addUser);
+
+
+// delete user
+router.delete('/:id', removeUser);
 
 module.exports = router;
